742ca03f98c7110bef7613ac2a36d93de673243c
vazenin <cnikromen@gmail.com>
2025-11-22 13:13:26 +0300

    feat: :sparkles: create tables

--- hw01/logistics-service-db/up/20251121192044_create_tables.sql
+++ hw01/logistics-service-db/up/20251121192044_create_tables.sql
@@ -1,1 +1,179 @@
 SELECT 'up SQL query';
+
+CREATE TABLE IF NOT EXISTS shipments (
+    -- PK
+    shipment_id SERIAL PRIMARY KEY,
+
+    -- UK: Business Key
+    shipment_external_id UUID NOT NULL UNIQUE,
+
+    order_external_id UUID NOT NULL,
+
+    -- UK: Трек-номер 
+    tracking_number VARCHAR(100) UNIQUE,
+    status VARCHAR(50),
+
+    weight_grams INTEGER,
+    volume_cubic_cm INTEGER,
+    package_count INTEGER,
+
+
+    origin_warehouse_code VARCHAR(50),
+    destination_type VARCHAR(50),
+    destination_pickup_point_code VARCHAR(50),
+
+    destination_address_external_id UUID,
+
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    dispatched_date TIMESTAMP, 
+    estimated_delivery_date TIMESTAMP, 
+    actual_delivery_date TIMESTAMP, 
+
+    delivery_notes TEXT,
+    recipient_name VARCHAR(255),
+    delivery_signature VARCHAR(255), 
+
+    effective_from TIMESTAMP,
+    effective_to TIMESTAMP,
+    is_current BOOLEAN DEFAULT TRUE,
+
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_by VARCHAR(100)
+);
+
+CREATE INDEX IF NOT EXISTS idx_shipments_order_ext_id ON shipments(order_external_id);
+CREATE INDEX IF NOT EXISTS idx_shipments_address_ext_id ON shipments(destination_address_external_id);
+
+
+CREATE TABLE IF NOT EXISTS shipment_movements (
+    movement_id SERIAL PRIMARY KEY,
+
+    -- FK: Связь с таблицей shipments
+    shipment_external_id UUID NOT NULL,
+
+    movement_type VARCHAR(50), 
+    location_type VARCHAR(50), 
+    location_code VARCHAR(50),
+    movement_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    operator_name VARCHAR(100),
+    notes TEXT,
+
+    latitude DECIMAL(10, 8),
+    longitude DECIMAL(11, 8),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR(100),
+
+    CONSTRAINT fk_movements_shipment 
+        FOREIGN KEY (shipment_external_id) 
+        REFERENCES shipments (shipment_external_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_movements_shipment_id ON shipment_movements(shipment_external_id);
+
+CREATE TABLE IF NOT EXISTS shipment_status_history (
+    history_id SERIAL PRIMARY KEY,
+
+    -- FK: Связь с таблицей shipments
+    shipment_external_id UUID NOT NULL,
+
+    old_status VARCHAR(50),
+    new_status VARCHAR(50),
+    change_reason VARCHAR(255),
+    
+    changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    changed_by VARCHAR(100),
+    
+    location_type VARCHAR(50),
+    location_code VARCHAR(50),
+    
+    notes TEXT,
+    customer_notified BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT fk_status_history_shipment 
+        FOREIGN KEY (shipment_external_id) 
+        REFERENCES shipments (shipment_external_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_status_history_shipment_id ON shipment_status_history(shipment_external_id);
+
+CREATE TABLE IF NOT EXISTS warehouses (
+    -- PK
+    warehouse_id SERIAL PRIMARY KEY,
+
+    -- UK: Business Key
+    warehouse_code VARCHAR(50) NOT NULL UNIQUE,
+
+    warehouse_name VARCHAR(100) NOT NULL,
+    warehouse_type VARCHAR(50), 
+
+    country VARCHAR(100),
+    region VARCHAR(100),
+    city VARCHAR(100),
+    street_address VARCHAR(255),
+    postal_code VARCHAR(20),
+
+    is_active BOOLEAN DEFAULT TRUE,
+
+    max_capacity_cubic_meters DECIMAL(15, 2),
+
+    operating_hours VARCHAR(255), 
+    contact_phone VARCHAR(50),
+    manager_name VARCHAR(100),
+
+    effective_from TIMESTAMP,
+    effective_to TIMESTAMP,
+    is_current BOOLEAN DEFAULT TRUE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_by VARCHAR(100)
+);
+
+
+CREATE INDEX IF NOT EXISTS idx_warehouses_city ON warehouses(city);
+CREATE INDEX IF NOT EXISTS idx_warehouses_is_active ON warehouses(is_active);
+
+
+CREATE TABLE IF NOT EXISTS pickup_points (
+    -- PK
+    pickup_point_id SERIAL PRIMARY KEY,
+
+    -- UK: Business Key 
+    pickup_point_code VARCHAR(50) NOT NULL UNIQUE,
+
+    pickup_point_name VARCHAR(100) NOT NULL,
+    pickup_point_type VARCHAR(50), 
+
+    country VARCHAR(100),
+    region VARCHAR(100),
+    city VARCHAR(100),
+    street_address VARCHAR(255),
+    postal_code VARCHAR(20),
+
+    is_active BOOLEAN DEFAULT TRUE,
+
+    max_capacity_packages INTEGER,
+
+    operating_hours VARCHAR(255),
+    contact_phone VARCHAR(50),
+    partner_name VARCHAR(100),
+    
+    effective_from TIMESTAMP,
+    effective_to TIMESTAMP,
+    is_current BOOLEAN DEFAULT TRUE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_by VARCHAR(100)
+);
+
+CREATE INDEX IF NOT EXISTS idx_pickup_points_city ON pickup_points(city);
+CREATE INDEX IF NOT EXISTS idx_pickup_points_type ON pickup_points(pickup_point_type);
--- hw01/order-service-db/up/20251121192033_create_tables.sql
+++ hw01/order-service-db/up/20251121192033_create_tables.sql
@@ -1,1 +1,137 @@
 SELECT 'up SQL query';
+
+
+CREATE TABLE IF NOT EXISTS ORDERS (
+    order_id SERIAL PRIMARY KEY,
+    
+    -- Business Key самого заказа
+    order_external_id UUID NOT NULL UNIQUE,
+
+    user_external_id UUID NOT NULL, 
+
+    order_number VARCHAR(100) NOT NULL UNIQUE,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50),
+    
+    subtotal DECIMAL(15, 2),
+    tax_amount DECIMAL(15, 2),
+    shipping_cost DECIMAL(15, 2),
+    discount_amount DECIMAL(15, 2),
+    total_amount DECIMAL(15, 2),
+    currency VARCHAR(3),
+
+    delivery_address_external_id UUID,
+
+    delivery_type VARCHAR(50),
+    expected_delivery_date DATE,
+    actual_delivery_date DATE,
+    payment_method VARCHAR(50),
+    payment_status VARCHAR(50),
+
+    effective_from TIMESTAMP,
+    effective_to TIMESTAMP,
+    is_current BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_by VARCHAR(100)
+);
+
+CREATE INDEX IF NOT EXISTS idx_orders_user_ext_id ON ORDERS(user_external_id);
+
+CREATE TABLE IF NOT EXISTS ORDERS_STATUS_HISTORY (
+    -- PK
+    history_id SERIAL PRIMARY KEY,
+
+    -- FK: Ссылка на таблицу orders (в этой же БД)
+    order_external_id UUID NOT NULL,
+
+    old_status VARCHAR(50),
+    new_status VARCHAR(50),
+    change_reason VARCHAR(255),
+    
+    changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    changed_by VARCHAR(100),
+    session_id VARCHAR(100),
+    
+    ip_address INET,
+
+    notes TEXT,
+
+    CONSTRAINT fk_order_history_order 
+        FOREIGN KEY (order_external_id) 
+        REFERENCES orders (order_external_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_order_history_order_ext_id 
+    ON ORDERS_STATUS_HISTORY(order_external_id);
+
+CREATE TABLE IF NOT EXISTS ORDER_ITEMS (
+    -- PK
+    order_item_id SERIAL PRIMARY KEY,
+
+    -- FK: Ссылка на таблицу orders
+    order_external_id UUID NOT NULL,
+
+    product_sku VARCHAR(100) NOT NULL,
+
+    quantity INTEGER NOT NULL,
+    
+    unit_price DECIMAL(15, 2) NOT NULL,
+    total_price DECIMAL(15, 2) NOT NULL,
+
+    product_name_snapshot VARCHAR(255),
+    product_category_snapshot VARCHAR(100),
+    product_brand_snapshot VARCHAR(100),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_by VARCHAR(100),
+
+    CONSTRAINT fk_order_items_order 
+        FOREIGN KEY (order_external_id) 
+        REFERENCES orders (order_external_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_order_items_order_id ON ORDER_ITEMS(order_external_id);
+
+CREATE INDEX IF NOT EXISTS idx_order_items_product_sku ON ORDER_ITEMS(product_sku);
+
+CREATE TABLE IF NOT EXISTS PRODUCTS (
+    -- PK: Суррогатный ключ
+    product_id SERIAL PRIMARY KEY,
+
+    -- UK: Business Key (Артикул товара)
+    product_sku VARCHAR(100) NOT NULL UNIQUE,
+
+    product_name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    brand VARCHAR(100),
+
+    price DECIMAL(15, 2),
+    currency VARCHAR(3), 
+
+    weight_grams INTEGER,
+    dimensions_length_cm DECIMAL(10, 2),
+    dimensions_width_cm DECIMAL(10, 2),
+    dimensions_height_cm DECIMAL(10, 2),
+
+    is_active BOOLEAN DEFAULT TRUE,
+
+    effective_from TIMESTAMP,
+    effective_to TIMESTAMP,
+    is_current BOOLEAN DEFAULT TRUE,
+
+    -- Audit Columns
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_by VARCHAR(100)
+);
+
+CREATE INDEX IF NOT EXISTS idx_products_category ON PRODUCTS(category);
+CREATE INDEX IF NOT EXISTS idx_products_brand ON PRODUCTS(brand);
+CREATE INDEX IF NOT EXISTS idx_products_is_active ON PRODUCTS(is_active);
